16961e4ac0df202772d335073d278e7cd4b2a1c8
wangjiwang <xautwjw@gmail.com>
2018-11-22 15:49:08 +0800

    Arduino/bluetooth
    bluetooth is a test about arduino use HC-06 to communicate with phone ;

---
+++ blueTooth.ino
@@ -0,0 +1,20 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  while(Serial.available()){ 
+    int c=Serial.read();
+    if(c==97) { 
+    Serial.println("BT is ready!"); 
+    Serial.write("Serial--13--high"); // 返回到手机调试程序上 
+    digitalWrite(13, HIGH); 
+    } 
+    if(c==98) { 
+      Serial.write("Serial--13--low"); 
+      digitalWrite(13, LOW); 
+     } 
+   }
+  }
+
