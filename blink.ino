bf97298dcc39edc2a3bf80f3155b86542c456e74
wangjiwang <xautwjw@gmail.com>
2018-11-22 15:47:11 +0800

    Arduino/blink
    blink.ino is use arduino and epoc+ to control light, blink twice and you can turn on the light, blink once you can turn off the light

---
+++ blink.ino
@@ -0,0 +1,36 @@
+int led = 13;
+int button;
+int flag =0;
+unsigned long starttime;
+unsigned long stoptime;
+unsigned long looptime;
+void setup() {
+  // put your setup code here, to run oncbbe
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  if(Serial.available()>0){
+    button = Serial.read();
+    starttime = millis();
+    Serial.print("the starttime");
+    Serial.println(starttime);
+    
+    delay(1000);
+   
+    if(Serial.available()>0){
+      digitalWrite(led,HIGH);
+      button = Serial.read();
+      stoptime = millis();
+      Serial.print("the stoptime");
+      Serial.println(stoptime);
+      
+     looptime = stoptime - starttime;
+     Serial.println(looptime);
+   }
+   else{
+    digitalWrite(led,LOW);
+   }
+ }
+}
+
